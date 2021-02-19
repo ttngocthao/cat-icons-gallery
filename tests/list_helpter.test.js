e33100a07013ad8ea1430269b7d84363b8e5770d
ttngocthao <ttngocthao_87@yahoo.com>
2021-02-19 09:12:47 +0000

    fixed mostblog function

--- tests/list_helpter.test.js
+++ tests/list_helpter.test.js
@@ -1,5 +1,55 @@
 const listHelper = require('../utils/list_helper')
 
+const defaultBlogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: "5a422b891b54a676234d17fa",
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: "5a422ba71b54a676234d17fb",
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+    __v: 0
+  }  
+]
 
 test('dummy returns one',()=>{
     const blogs=[]
@@ -30,14 +80,8 @@ describe('total likes',()=>{
     })
 
     test('of a bigger list is calculated right',()=>{
-        const blogs =[
-            {title: 'This is 1st blog', author: 'Thao', url: 'url',likes: 6 },
-            {title: 'This is 2nd blog', author: 'Thao', url: 'url',likes: 4 },
-            {title: 'This is 3rd blog', author: 'Thao', url: 'url',likes: 2 },
-            {title: 'This is 4th blog', author: 'Thao', url: 'url',likes: 3 },
-        ]
-        const result = listHelper.totalLikes(blogs)
-        expect(result).toBe(15)
+        const result = listHelper.totalLikes(defaultBlogs)
+        expect(result).toBe(36)
     })
 })
 
@@ -49,53 +93,20 @@ describe('finds out which blog has most likes',()=>{
         expect(result).toEqual({})
     })
 
-    test('blog list has 2 items, returns the blog with highest likes',()=>{
-        const blogs = [
-            {
-                title: "Canonical string reduction",
-                author: "Edsger W. Dijkstra",
-                likes: 12
-            },
-            {
-                title: "Canonical string reduction",
-                author: "Edsger W. Dijkstra",
-                likes: 6
-            },
-        ]
-        const result = listHelper.favoriteBlog(blogs)
+    test('returns the blog with highest likes',()=>{
+        
+        const result = listHelper.favoriteBlog(defaultBlogs)
         expect(result).toEqual({
-                title: "Canonical string reduction",
-                author: "Edsger W. Dijkstra",
-                likes: 12
-            })
+            _id: "5a422b3a1b54a676234d17f9",
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+            __v: 0
+        })
     })
 
-    test('blog list has more than 2 items, returns the blog with highest likes',()=>{
-        const blogs = [
-             {
-                title: "Canonical string reduction",
-                author: "Edsger W. Dijkstra",
-                likes: 4
-            },
-            {
-                title: "Canonical string reduction",
-                author: "Edsger W. Dijkstra",
-                likes: 12
-            },
-            {
-                title: "Canonical string reduction",
-                author: "Edsger W. Dijkstra",
-                likes: 6
-            },
-        ]
-        const result = listHelper.favoriteBlog(blogs)
-    
-        expect(result).toEqual({
-                title: "Canonical string reduction",
-                author: "Edsger W. Dijkstra",
-                likes: 12
-            })
-    })
+   
 
     test('blog list has more than 2 items and many top favorites, returns one of them',()=>{
          const blogs = [
@@ -133,25 +144,24 @@ describe('finds author who has the largest amount of blogs in an array of blogs'
     })
 
     test('returns an object (with author and blogs) of the author who has the largest amount of blogs',()=>{
-        const blogs=[
-            {id:'1',author:'Robert',blogs:3},
-            {id:'2',author:'Candy',blogs:15},
-            {id:'3',author:'John',blogs:8},
-        ]
-        const result = listHelper.mostBlogs(blogs)
-        console.log(result)
-        debugger
-        expect(result).toEqual({id:'2',author:'Candy',blogs:15})
+       
+        const result = listHelper.mostBlogs(defaultBlogs)
+        
+        expect(result).toEqual({author:'Robert C. Martin',blogs:3})
     })
 
     test('if there are many top blogges, return any of them',()=>{
         const blogs=[
-            {id:'1',author:'Robert',blogs:3},
-            {id:'2',author:'Candy',blogs:8},
-            {id:'3',author:'John',blogs:8},
-            {id:'3',author:'Jane',blogs:5},
+           ...defaultBlogs, {
+                _id: "5a422aa71b54a676234d17f8",
+                title: "Go To Statement Considered Harmful",
+                author: "Edsger W. Dijkstra",
+                url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+                likes: 5,
+                __v: 0
+            },
         ]
          const result = listHelper.mostBlogs(blogs)
-        expect(result).toEqual({id:'2',author:'Candy',blogs:8})
+        expect(result).toEqual({author:'Edsger W. Dijkstra',blogs:3})
     })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -28,7 +28,17 @@ const mostBlogs =(blogsList)=>{
     if(blogsList.length===0){
         return {}
     }else{
-       const mostBlogs =  blogsList.reduce((pre,current)=>{
+        //get an array of author
+        const authors = blogsList.map(blog=>blog.author)
+        //get an object {authorName: 2, authorName: 5} - count how many time that author name appear
+        const countBlogs = authors.reduce((result,author)=>{
+            result[author] = (result[author]||0)+1
+            return result
+        },{})
+       //turn the object above to an array of object [{author: name, blogs:3},{author:name, blogs:5}]
+        const authorsWithBlogsCountArr = Object.keys(countBlogs).map(key=>({author: key,blogs:countBlogs[key]}))
+        //find the author with the most blogs count
+       const mostBlogs =  authorsWithBlogsCountArr.reduce((pre,current)=>{
             if(pre.blogs === current.blogs){
                 return pre
             }else{
