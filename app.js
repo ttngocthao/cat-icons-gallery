20acdf19ea6cc0e8bb9aa7ea37262ca3aae67aea
ttngocthao <ttngocthao_87@yahoo.com>
2021-03-04 08:37:06 +0000

    added login router

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const logger = require('./utils/logger')
 
 const blogsRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 if(process.env.NODE_ENV!=='test'){
     mongoose.connect(config.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
@@ -24,6 +25,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs',blogsRouter)
 app.use('/api/users',userRouter)
+app.use('/api/login',loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
---
+++ controllers/login.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/User')
+
+loginRouter.post('/',async(req,res)=>{
+    /**
+     *? get username and password from req.body
+     *? check if username exists
+     *? check password matches
+     *? if username and password are correct, generates token with the response
+     */
+    const body = req.body
+    const user =await User.findOne({username:body.username})
+    const passwordCorrect = user===null ? false : await bcrypt.compare(body.password,user.passwordHash)
+
+    if(!(user && passwordCorrect)){
+        res.status(401)
+        throw Error('Invalid username or password')
+    }
+
+    //use username and id info to generate token
+    const userForToken ={
+        username: user.username,
+        id: user._id
+    }
+
+    const token = jwt.sign(userForToken,process.env.TOKEN_SECRET)
+
+    res.status(200).send({token,username: user.username,name: user.name})
+})
+
+module.exports = loginRouter
