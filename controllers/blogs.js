7eae636df9ffa497454b2de7c110d2bc30916333
ttngocthao <ttngocthao_87@yahoo.com>
2021-03-09 08:26:51 +0000

    delete api with auth works

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -9,7 +9,7 @@ path: /api/blogs
  */
 blogsRouter.get('/', async(request, response) => {
   const blogs = await Blog.find({}).populate('user',{username:1,name:1})   
-  console.log('blogs',blogs)
+  //console.log('blogs',blogs)
   response.status(200).json(blogs.map(blog=>blog.toJSON()))
     
 })
@@ -38,9 +38,7 @@ blogsRouter.post('/', async(request, response) => {
   
 
   const decodedToken = jwt.verify(request.token,process.env.TOKEN_SECRET)
-  // console.log('token',token)
-  // console.log('decodedToken',decodedToken)
-  
+
   if(!request.token || !decodedToken.id){
     return response.status(401).json({error: 'Token is missing or invalid'})
   }
@@ -68,14 +66,45 @@ blogsRouter.post('/', async(request, response) => {
 })
 
 blogsRouter.delete('/:id',async(request,response)=>{
+ const decodedToken = jwt.verify(request.token,process.env.TOKEN_SECRET)
+
+  if(!request.token || !decodedToken.id){
+    return response.status(401).json({error: 'Token is missing or invalid'})
+  }
+
+  const body = request.body
+  const user = await User.findById(decodedToken.id)
+  
+  if(!user){
+    return response.status(400)
+    //throw Error('User cannot be found')
+  }
+  /**
+   * TODO --> find the blog --> check if blog exist
+   * TODO --> check if user is the same with the post creator
+   * TODO --> if so allow delete
+   * TODO --> if not send Unauthorized status error
+   */
+  const blog = await Blog.findById(request.params.id)
+  if(!blog){
+    return response.status(400).json({error:'blog does not exist'})
+  }
+  if(blog.user.toString()!==user._id.toString()){
+    return response.status(401).json({error:'No Authorization to delete the blog'})
+  }
+  const result = await blog.deleteOne()
+  console.log('result',result)
+  
+  return response.status(204).json({msg: 'blog is successfully deleted'})
  
-  const result = await Blog.findByIdAndRemove(request.params.id,(err,doc)=>{
-    if(doc){
-      response.status(204).end()
-    }else{
-       response.status(400).end()
-    }
-  })
+ 
+  // const result = await Blog.findByIdAndRemove(request.params.id,(err,doc)=>{
+  //   if(doc){
+  //     response.status(204).end()
+  //   }else{
+  //      response.status(400).end()
+  //   }
+  // })
  
   
 })
